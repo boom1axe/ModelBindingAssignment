19b0dc0f4ec1c2dec1941ae1d76712fd77146805
Dennis Kafton <boom1axe@att.net>
2019-07-27 12:51:21 -0700

    Added starter class toy and database helper class toyDb

---
+++ ModelBindingAssignment/Models/Toy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBindingAssignment.Models
+{
+    public class Toy
+    {
+        public int ToyID { get; set; }
+        public String Name { get; set; }
+        public String discription { get; set; }
+        public String color { get; set; }
+        public String Company { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ ModelBindingAssignment/Models/ToyDb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBindingAssignment.Models
+{
+    public class ToyDb
+    {
+        public void Register(Toy t)
+        throw new NotImplementedException();
+    }
+}
