99b327eb359ac773666b50d6e32ab6ec525b3af3
Dennis Kafton <boom1axe@att.net>
2019-07-28 21:05:05 -0700

    linked ToyDB database to Toy Db helper and Toy Class

--- ModelBindingAssignment/Models/Toy.cs
+++ ModelBindingAssignment/Models/Toy.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ModelBindingAssignment.Models
 {
+    /// <summary>
+    /// data of a toy
+    /// </summary>
     public class Toy
     {
+        /// <summary>
+        /// ToyDB key
+        /// </summary>
         public int ToyID { get; set; }
-        public String Name { get; set; }
-        public String discription { get; set; }
-        public String color { get; set; }
+
+        /// <summary>
+        /// Legal brand name of toy
+        /// </summary>
+        [Required(ErrorMessage = "Toy brand name required")]
+        [Display(Name = "Toy Brand Name")]
+        public String Title { get; set; }
+
+        /// <summary>
+        /// Short description of toy or other info
+        /// </summary>
+        public String Description { get; set; }
+
+        /// <summary>
+        /// Color of a toy
+        /// Only Required for toys 
+        /// that share a brand name but
+        /// has more then one color
+        /// </summary>
+        public String Color { get; set; }
+        [Required]
         public String Company { get; set; }
-        public string type { get; set; }
+        public string Type { get; set; }
     }
 }
--- ModelBindingAssignment/Models/ToyDb.cs
+++ ModelBindingAssignment/Models/ToyDb.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ModelBindingAssignment.Models
 {
-    public class ToyDb
+    public static class ToyDb
     {
-        public void Register(Toy t)
-        throw new NotImplementedException();
+        public static void Register(Toy t)
+        {
+            SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=ToyDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO Toy (Title,Description,Color,Company,Type)" +
+                " VALUES (@Title,Description,Color,Company,Type)";
+            cmd.Parameters.AddWithValue("@Title", t.Title);
+            cmd.Parameters.AddWithValue("@Description", t.Description);
+            cmd.Parameters.AddWithValue("@Color", t.Color);
+            cmd.Parameters.AddWithValue("@Company", t.Company);
+            cmd.Parameters.AddWithValue("@Type", t.Type);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Dispose();
+            }
+        }
+
+        
     }
 }
