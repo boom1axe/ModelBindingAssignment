2055b7a98099615f898f4f2c7f1975319499d3fa
Dennis Kafton <boom1axe@att.net>
2019-07-28 22:20:22 -0700

    Started controller toy

---
+++ ModelBindingAssignment/Controllers/ToyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingAssignment.Controllers
+{
+    public class ToyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Catalog(IFormCollection data)
+        {
+            Toy t = new Toy()
+        }
+    }
+}
